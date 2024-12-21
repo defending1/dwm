ace8feedeb46945263d58fa3d9e120202db76248
Alberto Defendi <albedef@protonmail.com>
2024-12-21 16:30:05 +0100

    Apply swallow patch.

--- config.def.h
+++ config.def.h
@@ -3,8 +3,10 @@
 /* appearance */
 static const unsigned int borderpx = 1; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
-static const int showbar = 1;           /* 0 means no bar */
-static const int topbar = 1;            /* 0 means bottom bar */
+static const int swallowfloating =
+    0;                        /* 1 means swallow floating windows by default */
+static const int showbar = 1; /* 0 means no bar */
+static const int topbar = 1;  /* 0 means bottom bar */
 static const char *fonts[] = {
     "Hack Nerd Font:size=12:antialias=true:autohint=true",
     "Noto Color Emoji:antialias=true:autohint=true"};
@@ -48,10 +50,13 @@ static const Rule rules[] = {
      *	WM_CLASS(STRING) = instance, class
      *	WM_NAME(STRING) = title
      */
-    /* class      instance    title       tags mask     isfloating   monitor */
-    {"Gimp", NULL, NULL, 0, 1, -1},
-    {"Firefox", NULL, NULL, 1 << 8, 0, -1},
-    {NULL, NULL, "stfloat", 0, 1, -1},
+    /* class     instance  title           tags mask  isfloating  isterminal
+       noswallow  monitor */
+    {"Gimp", NULL, NULL, 0, 1, 0, 0, -1},
+    {"Firefox", NULL, NULL, 1 << 8, 0, 0, -1, -1},
+    {"St", NULL, NULL, 0, 0, 1, 0, -1},
+    {NULL, NULL, "Event Tester", 0, 0, 0, 1, -1}, /* xev */
+    {NULL, NULL, "stfloat", 0, 1, 1, 0, -1},
 };
 
 /* layout(s) */
